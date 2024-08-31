05518bb8bfad942e660e3fd42cf64d8ee13ad0d1
Ailton Souza <ailton.d3v@gmail.com>
2024-08-31 18:43:16 -0300

    feat: Separando e Unindo Strings

--- basic/extracting_string.py
+++ basic/extracting_string.py
@@ -2,3 +2,13 @@ texto = "Python é legal"
 print(texto[0:6])
 print(texto[-5:])
 print(texto[4:len(texto)])
+
+
+texto = "Aprender Python é divertido"
+partes = texto.split()
+print(partes)
+print(f"{partes[1]} {partes[2]} {partes[3]}!")
+print('----------------------------------------')
+
+unido = " " . join(partes)
+print(unido)
