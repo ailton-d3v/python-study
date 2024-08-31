88659f41823b2bba7c156e447c3463b5982b8752
Ailton Souza <ailton.d3v@gmail.com>
2024-08-31 18:54:44 -0300

    feat: Realizando operações matemáticas

---
+++ basic/data_entry.py
@@ -0,0 +1,19 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+print(f"Olá, {nome}. Você tem {idade} anos.")
+
+valor1 = float(input("Digite o primeiro valor: "))
+valor2 = float(input("Digite o segundo valor: "))
+
+soma = valor1 + valor2
+subtracao = valor1 - valor2
+multiplicacao = valor1 * valor2
+divisao = valor1 / valor2
+exponenciacao = valor1 ** valor2
+modulo = valor1 % valor2
+print(soma)
+print(subtracao)
+print(multiplicacao)
+print(divisao)
+print(exponenciacao)
+print(modulo)
