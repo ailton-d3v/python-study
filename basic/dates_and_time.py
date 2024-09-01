c301e3abea36f2b124eab9b457cabf5612f4c256
Ailton Souza <ailton.d3v@gmail.com>
2024-09-01 17:13:03 -0300

    feat: adiciona ou subtrai dias, horas, etc.

--- basic/dates_and_time.py
+++ basic/dates_and_time.py
@@ -8,3 +8,7 @@ print(agora)
 # criar uma data especifica
 data = datetime(2024, 9, 4, 17, 40, 30, 121554)
 print(data)
+
+# adiciona ou subtrai dias, horas...
+futuro = agora + timedelta(days=2)
+print(futuro)
