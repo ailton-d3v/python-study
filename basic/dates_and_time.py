679fb4296c1cba1d85a442625d6bf24c9acc2476
Ailton Souza <ailton.d3v@gmail.com>
2024-09-01 17:10:57 -0300

    feat: Cria uma data especifica

--- basic/dates_and_time.py
+++ basic/dates_and_time.py
@@ -3,3 +3,8 @@ from datetime import datetime, timedelta
 # data e hora atual
 agora = datetime.now()
 print(agora)
+
+
+# criar uma data especifica
+data = datetime(2024, 9, 4, 17, 40, 30, 121554)
+print(data)
